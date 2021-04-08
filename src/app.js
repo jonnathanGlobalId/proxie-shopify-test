2701dfe4bcf8124d66455d321a8a6b98c2ba1839
Miguel Jardinez Ramos <miguel.jardinez@global.id>
2021-04-08 13:20:19 -0500

    add public html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const path = require('path');
 dotenv.config();
 
-app.get('/', (req, res) => {
-  res.json({
-    mensaje: 'Listo en el ersvidor de shopify',
-  })
-});
+const proxyRoute = require('./routes/proxyRoutes');
+
+//utilizar Paginas estaticas
+app.use(express.static('public'));
+
+//Rutas para la aplicación en heroku
+app.use('/', proxyRoute);
 
 app.listen(process.env.PORT, () => {
   console.log('servidor listo en el PORT', process.env.PORT);
