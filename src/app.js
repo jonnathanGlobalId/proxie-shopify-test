049fe04286d806a71c7dcbfd14c9c0e90fc15435
Miguel Jardinez Ramos <miguel.jardinez@global.id>
2021-04-08 10:58:28 -0500

    send data in server

--- src/app.js
+++ src/app.js
@@ -4,11 +4,9 @@ const app = express();
 dotenv.config();
 
 app.get('/', (req, res) => {
-  res.json({
-    mensaje: 'La base de la api'
-  });
+  res.send('Listo en el servidor');
 });
 
-app.listen(process.env.PUERTO, () => {
-  console.log('servidor listo en el puerto', process.env.PUERTO);
+app.listen(process.env.PORT, () => {
+  console.log('servidor listo en el PORT', process.env.PORT);
 })
