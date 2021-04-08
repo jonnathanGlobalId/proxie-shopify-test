bdd89e02cb047b6903d09fcf42ce6e66ec9d60ee
Miguel Jardinez Ramos <miguel.jardinez@global.id>
2021-04-08 09:46:37 -0500

    start api finished

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.json({
+    mensaje: 'La base de la api esta funcionando'
+  });
+});
+
+app.listen(process.env.PUERTO, () => {
+  console.log('servidor listo en el puerto', process.env.PUERTO);
+})
