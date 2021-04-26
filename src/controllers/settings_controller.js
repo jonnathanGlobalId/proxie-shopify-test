b1ec1f969f12ef14a0524efcca8c977516accf84
Miguel Jardinez Ramos <miguel.jardinez@global.id>
2021-04-26 14:02:34 -0500

    add new shopify config

--- src/controllers/settings_controller.js
+++ src/controllers/settings_controller.js
@@ -1,9 +1,5 @@
 
 let user_settings = {
-  uuid: '328u0293eu023ue2309',
-  global_client_id: '1234567890',
-  shop_name: 'The krusty krab',
-  photo_url: 'https://graffica.info/wp-content/uploads/2017/08/LogoNasaSpotB-1200x900.jpg',
   limit_ammount: 100,
   settings: {
     address: true,
