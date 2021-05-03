46a56961e8e129d427f51fa97f05f0d73d30d01a
Miguel Jardinez Ramos <miguel.jardinez@global.id>
2021-05-03 17:06:33 -0500

    add middleware to check hemac

--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const {getSettings, changeSettings} = require('../controllers/settings_controller');
 const {getQuery} = require('../controllers/shopifyAccess');
 const {getauthInfo, sendAuthInfo} = require('../controllers/auth_controller');
+const {checkHmac} = require('../middlewares/verifyHmac');
 
 router.get('/shopify-access', getQuery);
-router.get('/user-settings', getSettings);
+router.get('/user-settings', checkHmac, getSettings);
 router.post('/user-settings', changeSettings);
 
 //Access token de usuario
