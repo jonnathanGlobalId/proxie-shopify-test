bad05e8f6fe113e5ea3ef7711e2dbb3a95980c4a
Miguel Jardinez Ramos <miguel.jardinez@global.id>
2021-05-26 12:05:41 -0500

    fix typo

--- public/script-tag.js
+++ public/script-tag.js
@@ -31,10 +31,11 @@ function handler() {
   //   .then((res) => res.json())
   //   .then((data) => console.log(data))
   //   .catch((e) => console.log(e))
-  // fetch(`https://proxy-script-shopify.herokuapp.com/api/shopify-access?shop=${shop}`)
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((e) => console.log(e))
+
+  fetch(`https://shopify-fake-api.herokuapp.com/api/shopify-access?shop=${shop}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((e) => console.log(e))
 
   fetch(`https://shopify-fake-api.herokuapp.com/api/user-settings?shop=${shop}`)
       .then((res) => res.json())
