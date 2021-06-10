6a211fb7d4a8e8f26fd7575e6faeb722f1a4c790
Miguel Jardinez <miguel_jardinez@hotmail.com>
2021-06-10 13:40:34 -0500

    send correct timestamp to globalid

--- public/script-tag.js
+++ public/script-tag.js
@@ -67,78 +67,73 @@ function handler() {
 
 
   const templateSettings = (settings) => {
-    if (Shopify?.checkout && arrayUrl.length > 2 && arrayUrl[2] === 'checkouts') {
-    
-        console.log('Desde la función', settings);
-        console.log('Tenemos informacion del checkout');
-        const address = Shopify.checkout.billing_address.address1 !== Shopify.checkout.shipping_address.address1 && settings?.different_address_enabled;
-        const ammount = Number(Shopify.checkout.subtotal_price) >= settings.order_amount_limit_enabled && settings?.order_amount_limit;
-        const customerName = Shopify?.checkout?.billing_address?.first_name;
+    console.log('Desde la función', settings);
+    console.log('Tenemos informacion del checkout');
+    const address = Shopify.checkout.billing_address.address1 !== Shopify.checkout.shipping_address.address1 && settings?.different_address_enabled;
+    const ammount = Number(Shopify.checkout.subtotal_price) >= settings.order_amount_limit_enabled && settings?.order_amount_limit;
+    const customerName = Shopify?.checkout?.billing_address?.first_name;
 
-        if (address || ammount) {
-      
-          const contentSquare = $(
-            `<div />`
-            ).css({
-              'height': '100vh',
-              'padding-top': '32px', 
-              'width': '100vw', 
-              'background-color': 'white', 
-              'position': 'absolute', 
-              'z-index': '99',
-          });
-      
-          const infoUser = $(
-            `<div>
-              <div style="display: flex; flex-direction: column">
-                  <h1 style="text-align: center; font-size: 22px; margin-bottom: 24px;">
-                    Hi ${customerName}
-                  </h1>
-                  <p style="text-align: center; font-size: 22px; margin-bottom: 24px">
-                    ID Verification is required to complete this purchase.
-                  </p> 
-                  <p style="text-align: center; font-size: 22px; margin-bottom: 24px">
-                    This requirement is in place to protect you from credit-card 
-                    fraud and ensure you are eligible to purchase this product. 
-                  </p>
-                  <p style="text-align: center; font-size: 22px; margin-bottom: 24px">
-                    We partner with GlobaliD to keep your data private, secure and 
-                    in your control every step of the way. 
-                  </p>
-                  <button
-                    id="globalBtn"
-                    style="background-color: #2563eb; padding: 16px 0px; width: 100%; border-radius: 32px; color: white;">
-                  Enviar a global id</button>
-              </div>
-            </div>`
+    if (address || ammount) {
+      const contentSquare = $(
+          `<div />`
           ).css({
-            'max-width': '600px',
-            'margin': 'auto',
-            'color': 'black',
-            'padding': '0px 40px'
-          });
+            'height': '100vh',
+            'padding-top': '32px', 
+            'width': '100vw', 
+            'background-color': 'white', 
+            'position': 'absolute', 
+            'z-index': '99',
+        });
       
-          content.append(contentSquare);
-          contentSquare.append(infoUser);
+        const infoUser = $(
+          `<div>
+            <div style="display: flex; flex-direction: column">
+                <h1 style="text-align: center; font-size: 22px; margin-bottom: 24px;">
+                  Hi ${customerName}
+                </h1>
+                <p style="text-align: center; font-size: 22px; margin-bottom: 24px">
+                  ID Verification is required to complete this purchase.
+                </p> 
+                <p style="text-align: center; font-size: 22px; margin-bottom: 24px">
+                  This requirement is in place to protect you from credit-card 
+                  fraud and ensure you are eligible to purchase this product. 
+                </p>
+                <p style="text-align: center; font-size: 22px; margin-bottom: 24px">
+                  We partner with GlobaliD to keep your data private, secure and 
+                  in your control every step of the way. 
+                </p>
+                <button
+                  id="globalBtn"
+                  style="background-color: #2563eb; padding: 16px 0px; width: 100%; border-radius: 32px; color: white;">
+                Enviar a global id</button>
+            </div>
+          </div>`
+        ).css({
+          'max-width': '600px',
+          'margin': 'auto',
+          'color': 'black',
+          'padding': '0px 40px'
+        });
+      
+        content.append(contentSquare);
+        contentSquare.append(infoUser);
 
 
-          const handlePostInfo = async () => {
-            try {
-              console.log('Mandando la información a globalID', window.location.href);
-              window.localStorage.setItem('link', window.location.href);
-              const order_id = Shopify.checkout.order_id;
-              const state = `${shopName}__${order_id}`;
-              window.location.replace(`https://connect.globalid.dev/?client_id=fd61f598-aeca-47a1-b379-d1356e4ecc50&response_type=code&scope=openid&redirect_uri=https://apps.globalid.dev/v1/shopify-plugin/order/add&login=false&acrc_id=86db6c94-443d-44af-ad9a-fa9e0eb3e1d2&state=${state}&nonce=<INSERT_NONCE_HERE>&document_id=tos pp`);
-            } catch (error) {
-              console.log(error);
-            }
-          };
+        const handlePostInfo = async () => {
+          try {
+            console.log('Mandando la información a globalID', window.location.href);
+            window.localStorage.setItem('link', window.location.href);
+            const order_id = Shopify.checkout.order_id;
+            const state = `${shopName}__${order_id}`;
+            window.location.replace(`https://connect.globalid.dev/?client_id=fd61f598-aeca-47a1-b379-d1356e4ecc50&response_type=code&scope=openid&redirect_uri=https://apps.globalid.dev/v1/shopify-plugin/order/add&login=false&acrc_id=86db6c94-443d-44af-ad9a-fa9e0eb3e1d2&state=${state}&nonce=<INSERT_NONCE_HERE>&document_id=tos pp`);
+          } catch (error) {
+            console.log(error);
+          }
+        };
 
-          //Función para redireccionar a pagina de globalID
-          const buttonGlobal = document.querySelector('#globalBtn');
-          buttonGlobal.addEventListener('click', () => handlePostInfo());
-
-        }
+        //Función para redireccionar a pagina de globalID
+        const buttonGlobal = document.querySelector('#globalBtn');
+        buttonGlobal.addEventListener('click', () => handlePostInfo());
       }
   }
 }
